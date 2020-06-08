b639afbc24579b1c7d083a2dd83a21f053bbaa89
Sachin Karki <karkisachin151@gmail.com>
2020-06-08 21:53:05 +1000

    minor

---
+++ resources/views/listing.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+@section('main_content')
+<div class="cognito">
+<script src="https://www.cognitoforms.com/s/92FnLQbyBEuUfmHP5PBc1A"></script>
+<script>Cognito.load("forms", { id: "1" });</script>
+</div>
+@endsection
