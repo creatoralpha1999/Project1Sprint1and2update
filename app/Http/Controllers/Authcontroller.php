7cb24885e469a2765ee8f46db86f2af082551d78
Rahul Niraula <rahul.niraula7@live.com>
2020-06-08 17:40:06 +0545

    added property

--- app/Http/Controllers/Authcontroller.php
+++ app/Http/Controllers/Authcontroller.php
@@ -9,23 +9,25 @@ use App\User;
 
 class Authcontroller extends Controller
 {
-    public function register(Request $request){
-        $data=[
-            'name'=>$request->get('name'),
-            'email'=>$request->get('email'),
-            'password'=>bcrypt($request->get('password')),
-            'register_as'=>$request->get('register_as')
+    public function register(Request $request)
+    {
+        $data = [
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => bcrypt($request->get('password')),
+            'register_as' => $request->get('register_as')
         ];
         User::create($data);
-        return redirect()->route('home')->with('flash_success','User registered succesfully');
+        return redirect()->route('home')->with('flash_success', 'User registered succesfully');
     }
 
-    public function login(Request $request){
-        $credentials=['email'=>$request->get('email'),'password'=>$request->get('password')];
-        if(Auth::attempt($credentials)){
-            return redirect()->route('loggedin')->with('flash_success','Login Successful');
-        }else{
-            return redirect()->route('home')->with('flash_success','Login Failed');
+    public function login(Request $request)
+    {
+        $credentials = ['email' => $request->get('email'), 'password' => $request->get('password')];
+        if (Auth::attempt($credentials)) {
+            return redirect()->route('loggedin')->with('flash_success', 'Login Successful');
+        } else {
+            return redirect()->route('home')->with('flash_success', 'Login Failed');
         }
     }
 }
---
+++ resources/views/list_property.blade.php
@@ -0,0 +1,21 @@
+@extends('layout')
+@section('main_content')
+<div class="container-fluid">
+    <div class="row">
+        @foreach($properties as $property)
+        <div class="col-md-4">
+            <div class="card" style="width: 18rem;">
+                <span class="badge badge-success">For {{$property->type}}</span>
+                <img class="card-img-top" src="{{asset('properties/'.$property->image)}}" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">{{$property->title}}</h5>
+                    <p class="card-text">{{$property->description}}</p>
+                    <p class="card-text">Price:: {{$property->price}}</p>
+                    <a href="#" class="btn btn-block btn-primary">Request Property</a>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
+@endsection
